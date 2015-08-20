ef652fe30f57c9ea8b6736937619b145c16e0b1c
GOUDA <4line.waon@gmail.com>
2015-08-20 09:11:24 +0000

    second

--- features/step_definitions/NewTopicsSteps.rb
+++ features/step_definitions/NewTopicsSteps.rb
@@ -3,7 +3,7 @@
   agent = Mechanize.new
  page = agent.get("http://www.yahoo.co.jp/")
  File.open('/usr/local/log/log.txt','a') do |file|
-  file.write puts page.search('table')[14].text
+  file.write page.search('table')[14].text
   file.write Time.now.strftime("%X")+"\n"
   file.close
  end
